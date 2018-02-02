ef35588a6c26a44c984f949bdf0b4974f12412b2
akalshamsi <akalshamsi@mail.csuchico.edu>
2018-02-01 19:47:59 -0800

    Describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,23 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
+string name;
+char grade;
+double gpa;
+int time; 
 
+std::cout<<"Enter a name: ";
+std::cin>>name;
+std::cout<<"Enter a grade: ";
+std::cin>>grade;
+std::cout<<"Enter a gpa: ";
+std::cin>>gpa;
+std::cout<<"Enter a time: ";
+std::cin>>time;
+std::cout<<"on monday at "<<time<<":00 AM,  "<<name<<" went to the class to see his math grade , his grade was " << grade << ". Then he went on his laptop to check his gpa. It was " << gpa << " He was happy and he went on and celebrated" <<std::endl;
   return 0;
 }
